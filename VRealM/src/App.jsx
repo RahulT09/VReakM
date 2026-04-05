406e6851caa5b5e866bcfb6e09f53bfb1e68cacc
Rahul <techgamert576@gmail.com>
2026-04-06 01:39:01 +0530

    Landing page built

--- VRealM/src/App.jsx
+++ VRealM/src/App.jsx
@@ -1,12 +1,25 @@
-
-import './App.css'
+import { Navbar } from "./components/Navbar.jsx";
+import { HeroSection } from "./components/HeroSection.jsx";
+import { FeatureSection } from "./components/FeatureSection.jsx";
+import { WorkFlow } from "./components/WorkFlow.jsx";
+import { Pricing } from "./components/Pricing.jsx";
+import { Testimonial } from "./components/Testimonial.jsx";
+import { Footer } from "./components/Footer.jsx";
 
 function App() {
   return (
     <>
-      <h1>Hello world</h1>
+      <Navbar />
+      <div className="max-w-7xl mx-auto pt-20 px-6">
+        <HeroSection />
+        <FeatureSection />
+        <WorkFlow />
+        <Pricing />
+        <Testimonial />
+        <Footer />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
